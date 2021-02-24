1dd8ef6e4946c70ae25d896d23a670fc3f4b6cf4
Kat Allee <kallee@butlercc.edu>
2021-02-24 10:43:36 -0600

    Messing with ZStack, colors, and gradient.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,65 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 20) {
-            HStack(spacing: 20) {
-                Text("0.0")
-                    .font(.largeTitle)
-                Text("0.1")
-                    .font(.largeTitle)
-                Text("0.2")
-                    .font(.largeTitle)
-            }
-            HStack(spacing: 20) {
-                Text("1.0")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                Text("1.1")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-                Text("1.2")
-                    .font(.largeTitle)
-                    .fontWeight(.black)
-            }
-            HStack(spacing: 20) {
-                Text("2.0")
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.blue)
-                Text("2.1")
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.blue)
-                Text("2.2")
-                    .fontWeight(.heavy)
-                    .foregroundColor(Color.blue)
-            }
+        ZStack {
+            // ZStack builds back to front
+            LinearGradient(gradient: Gradient(colors: [Color.red, Color.yellow]), startPoint: .bottomLeading, endPoint: .topTrailing).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            //Color.green.edgesIgnoringSafeArea(.all)
+            Color.black.frame(width: 200, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                .cornerRadius(20)
+            Text("As You Wish")
+                .foregroundColor(.white)
         }
-//        Form {
-//            HStack {
-//                VStack(alignment: .leading)/*(spacing: 10)*/ {
-//                    Text("Hello, Ananas!")//.padding()
-//                    Text("Pineapple")
-//                    Text("Piney apple")
-//                }
-//                Spacer() //pushes things as far apart as they can be
-//                Text("*")
-//            }
-//            VStack(alignment: .leading)/*(spacing: 10)*/ {
-//                Text("Hello, Ananas!")//.padding()
-//                Text("Pineapple")
-//                Text("Piney apple")
-//            }
-//            .padding()
-//            VStack(alignment: .leading)/*(spacing: 10)*/ {
-//                Text("Hello, Ananas!")//.padding()
-//                Text("Pineapple")
-//                Text("Piney apple")
-//            }
-//            .padding()
-//            ZStack {
-//                Text("first")
-//                Text("second")
-//            }
-//        }
     }
 }
 
