0343a481e33624fb02118654faa2105dc71144e7
Kat Allee <kallee@butlercc.edu>
2021-02-24 10:44:37 -0600

    Messing with colors, gradients, and stack.

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
         ZStack {
             // ZStack builds back to front
             LinearGradient(gradient: Gradient(colors: [Color.red, Color.yellow]), startPoint: .bottomLeading, endPoint: .topTrailing).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            //Color.green.edgesIgnoringSafeArea(.all)
+            //Color.green
             Color.black.frame(width: 200, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
                 .cornerRadius(20)
             Text("As You Wish")
